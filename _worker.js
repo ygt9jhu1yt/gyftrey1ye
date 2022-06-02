a42c363a63fc71a56971dbbd2bef5d607c99e2de
ygt9jhu1yt <106634512+ygt9jhu1yt@users.noreply.github.com>
2022-06-02 02:38:30 +0000

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,7 @@
 export default {
   async fetch(request, env) {
     let url = new URL(request.url);
-    if (url.pathname.startsWith('/')) {
+    if (url.pathname.startsWith('/0STUYXRB')) {
       url.hostname = 'blue-bread-f7f3.owmanyp.workers.dev'
       let new_request = new Request(url, request);
       return fetch(new_request);
